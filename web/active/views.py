0e941254dd820a903dbce350839b133abc41e3d6
zwt <zwt@qq.com>
2013-04-26 09:16:25 +0800

    one

---
+++ web/active/views.py
@@ -0,0 +1,15 @@
+# Create your views here.
+from django.shortcuts import render,redirect
+
+
+def index(req):
+
+
+
+    return render(req,'index.html',{})
+
+
+def uregist(req):
+    pass
+
+
---
+++ web/web/urls.py
@@ -0,0 +1,20 @@
+from django.conf.urls import patterns, include, url
+from django.conf import settings
+# Uncomment the next two lines to enable the admin:
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.contrib import admin
+admin.autodiscover()
+from active.views import *
+urlpatterns = patterns('',
+    # Examples:
+    # url(r'^$', 'web.views.home', name='home'),
+    # url(r'^web/', include('web.foo.urls')),
+    url(r'^statics/(?P<path>.*)$','django.views.static.serve',{'document_root':settings.STATIC_PATH}),
+    # Uncomment the admin/doc line below to enable admin documentation:
+    url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    # Uncomment the next line to enable the admin:
+    # url(r'^admin/', include(admin.site.urls)),
+    url(r'^index/$',index),
+    url(r'^register/$',uregist),
+)
